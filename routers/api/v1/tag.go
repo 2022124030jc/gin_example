760c1513b79e1971e3a9c193bc5757b000e49185
2022124030jc <3062257150@qq.com>
2025-06-14 17:54:49 +0800

    v1 api

---
+++ routers/api/v1/tag.go
@@ -0,0 +1,21 @@
+package v1
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// 获取标签列表
+func GetTags(c *gin.Context) {
+}
+
+// 新增文章标签
+func AddTag(c *gin.Context) {
+}
+
+// 编辑文章标签
+func EditTag(c *gin.Context) {
+}
+
+// 删除文章标签
+func DeleteTag(c *gin.Context) {
+}
